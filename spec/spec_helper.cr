2eb05831eec2d45ac76d54c4cd8c6d256bde5eba
William Le <will@place.technology>
2021-04-14 14:15:19 +0800

    chore(spec): update mock request for list_events

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -253,7 +253,7 @@ module SpecHelper
   end
 
   def mock_list_events
-    WebMock.stub(:get, "https://graph.microsoft.com/v1.0/users/foo@bar.com/calendar/calendarView?startDateTime=2020-01-01T00:00:00-00:00&endDateTime=2020-06-01T00:00:00-00:00").to_return(mock_event_query_json)
+    WebMock.stub(:get, "https://graph.microsoft.com/v1.0/users/foo@bar.com/calendar/calendarView?startDateTime=2020-01-01T00:00:00-00:00&endDateTime=2020-06-01T00:00:00-00:00&top=10000").to_return(mock_event_query_json)
   end
 
   def mock_batch_list_events
