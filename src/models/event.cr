b50300dc052412b27b3f5b205698b2f1d0314c86
w-le <will@place.tech>
2021-06-17 13:50:41 +0800

    fix(event/create): ensure .location matches .locations[]

--- src/models/event.cr
+++ src/models/event.cr
@@ -108,11 +108,14 @@ module Office365
 
       # As outlined in the example request
       # https://docs.microsoft.com/en-us/graph/api/user-post-events?view=graph-rest-1.0&tabs=http#request-1
-      @locations = locs = @attendees.compact_map { |a| Location.new(display_name: a.name) if a.type == AttendeeType::Resource }
       if typeof(location) == String
-        @location = Location.new(display_name: location)
-      elsif !locs.empty?
-        @location = Location.new(display_name: locs.compact_map(&.display_name).join("; "))
+        # Use the provided location
+        @location  = Location.new(display_name: location)
+        @locations = [@location]
+      else
+        # Generate location based on invited resources
+        @locations = @attendees.compact_map { |a| Location.new(display_name: a.name) if a.type == AttendeeType::Resource }
+        @location  = @locations.join("; ")
       end
 
       if recurrence
