5d89ad5159539cf877ccfa747741f779b2ce523c
tassja <xtassja@gmail.com>
2021-09-03 13:37:04 +1000

    fix(events): account for non error messages

--- src/models/event_query.cr
+++ src/models/event_query.cr
@@ -14,10 +14,12 @@ class Office365::EventQuery
         Event.from_json(event.to_json)
       end
       new(value: result)
-    else
+    elsif parsed_data.has_key?("error")
       parsed_error_message = parsed_data["error"]["message"]?
-      error_message = parsed_error_message ? "Query not in the correct query format\nOffice365 Error: #{parsed_error_message}" : "Query not in the correct query format"
+      error_message = parsed_error_message ? "Query not in the correct query format\nOffice365 Error: #{parsed_error_message}" : "Query not in the correct query format, no error message available"
       raise JSON::Error.new error_message
+    else
+      raise JSON::Error.new "Query not in the correct query format, no error message available"
     end
   end
 end
