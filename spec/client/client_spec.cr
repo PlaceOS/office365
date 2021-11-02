271c9b66116a81ccdb8e94dcb6a236555f3ebe2e
tassja <xtassja@gmail.com>
2021-11-02 12:50:44 +1100

    style: spelling mistake in comment

--- spec/client/client_spec.cr
+++ spec/client/client_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 describe Office365::Client do
   describe "#get_token" do
-    it "creates a deafult client" do
+    it "creates a default client" do
       SpecHelper.mock_client_auth
       client = Office365::Client.new(**SpecHelper.mock_credentials)
       client.get_token.access_token.should eq(SpecHelper.mock_token.access_token)
