6fa9402fb0a243ffa296c662e042d06a92eb3b0a
Mia Bennett <mia@chillfox.com>
2022-04-27 11:09:02 +0930

    feat(user): add models for user invitations

---
+++ src/models/invitation.cr
@@ -0,0 +1,34 @@
+module Office365
+  # https://docs.microsoft.com/en-us/graph/api/resources/invitation?view=graph-rest-1.0
+  class Invitation
+    include JSON::Serializable
+    include JSON::Serializable::Unmapped
+
+    @[JSON::Field(key: "invitedUserDisplayName")]
+    property display_name : String?
+
+    @[JSON::Field(key: "invitedUserEmailAddress")]
+    property email : String
+
+    @[JSON::Field(key: "invitedUserMessageInfo")]
+    property message : InvitationMessage?
+
+    @[JSON::Field(key: "sendInvitationMessage")]
+    property send_invitation : Bool?
+
+    @[JSON::Field(key: "inviteRedirectUrl")]
+    property redirect_url : String
+
+    @[JSON::Field(key: "inviteRedeemUrl")]
+    property redeem_url : String?
+
+    @[JSON::Field(key: "invitedUserType")]
+    property user_type : String?
+
+    @[JSON::Field(key: "status")]
+    property status : String?
+
+    @[JSON::Field(key: "invitedUser")]
+    property user : User?
+  end
+end
