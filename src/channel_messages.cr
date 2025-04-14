717d1cf7b6469065fa33533be6a8ef314678a70a
Ali Naqvi <syed.alinaqvi@gmail.com>
2025-04-14 12:44:50 +0800

    refactor(channel_messages): Added stautus check + raise exception with details

--- src/channel_messages.cr
+++ src/channel_messages.cr
@@ -33,26 +33,45 @@ module Office365::ChannelMessages
   end
 
   def list_channel_messages(response : HTTP::Client::Response)
-    ChatMessageList.from_json(response.body)
+    if response.success?
+      ChatMessageList.from_json(response.body)
+    else
+      raise "error listing channel messages #{response.status} (#{response.status_code}\n#{response.body}"
+    end
   end
 
   def get_channel_message(team_id : String, channel_id : String, message_id : String)
     request = graph_http_request(request_method: "GET", path: "/v1.0/teams/#{team_id}/channels/#{channel_id}/messages/#{message_id}")
     response = graph_request(request)
-    ChatMessage.from_json(response.body)
+    if response.success?
+      ChatMessage.from_json(response.body)
+    else
+      raise "error getting channel message #{response.status} (#{response.status_code}\n#{response.body}"
+    end
   end
 
   def send_channel_message(team_id : String, channel_id : String, message : String | IO)
     body = message.is_a?(IO) ? message.gets_to_end : message
     chat = ChatMessage.new(body: body)
     request = graph_http_request(request_method: "POST", path: "/v1.0/teams/#{team_id}/channels/#{channel_id}/messages", data: chat.to_json)
-    graph_request(request)
+    response = graph_request(request)
+
+    if response.success?
+      ChatMessage.from_json(response.body)
+    else
+      raise "error sending channel message #{response.status} (#{response.status_code}\n#{response.body}"
+    end
   end
 
   def send_channel_message(team_id : String, channel_id : String, message : String | IO, content_type : String)
     body = message.is_a?(IO) ? message.gets_to_end : message
     chat = ChatMessage.new(content: body, content_type: content_type)
     request = graph_http_request(request_method: "POST", path: "/v1.0/teams/#{team_id}/channels/#{channel_id}/messages", data: chat.to_json)
-    graph_request(request)
+    response = graph_request(request)
+    if response.success?
+      ChatMessage.from_json(response.body)
+    else
+      raise "error sending channel message #{response.status} (#{response.status_code}\n#{response.body}"
+    end
   end
 end
